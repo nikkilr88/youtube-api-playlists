894b30b33504416defa5202e9875c38195003451
Nicole <nicole.sattler@live.com>
2018-01-30 09:48:16 -0600

    Add comments and functions to open and close iframe.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const $videos = $('.videos');
 const $loadBtn = $('.loadMore');
+const $closeBtn = $('#close');
 
 let pageToken;
 
+// GET DATA
 axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {
         params: {
             key: keys.youtube,
@@ -18,28 +20,49 @@ $loadBtn.on('click', () => {
     loadMore(pageToken)
 })
 
+$closeBtn.on('click', () => {
+    $('.embed').fadeOut();
+    $('.overlay').fadeOut();
+    $('#vidDisplay').attr('src', '');
+;});
+
+$videos.on('click', '.watch', (e) => {
+    let $this = $(e.currentTarget);
+    showIframe($this.data('url'));
+})
+
+// SHOW VIDEO DIVS
 function displayData(data) {
     pageToken = data.nextPageToken;
     let divs = createVideo(data).join('');
     $videos.append(divs);
 }
 
+// SHOW IFRAME
+function showIframe(url) {
+   $('#vidDisplay').attr('src', url);
+   $('.embed').fadeIn();
+   $('.overlay').fadeIn()
+;}
+
+// CREATE VIDEO DIVS
 function createVideo(data) {
     return data.items.map(item => {
-        let url = `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`;
+        let url = `https://www.youtube.com/embed/${item.snippet.resourceId.videoId}`;
         return `
             <div class="video">
                 <img src="${item.snippet.thumbnails.medium.url}">
                 <section class="vidInfo">
                     <h3>${item.snippet.title}</h3>
                     <p>${item.snippet.description.substring(0,100)}...</p>
-                    <a href="${url}">Watch</a>
+                    <button data-url="${url}" class="watch">Watch</button>
                 </section>
             </div>
         `;
     })
 }
 
+// LOAD MORE VIDEO DIVS
 function loadMore(token) {
     if (pageToken) {
         axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {
